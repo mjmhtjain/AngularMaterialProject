c37474e99e8fdf4ade270b3b042d76e94a3ea938
Jain Mohit (RBEI/ETC1) <ojn5kor@bosch.com>
2018-10-11 18:51:18 +0530

    cards layout prepared.. Images to be incorporated

--- src/app/shopping-dash/shopping-dash.component.ts
+++ src/app/shopping-dash/shopping-dash.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingDashComponent implements OnInit {
 
+  private beerList = [
+    {
+      productName: 'Corona Extra',
+      productInfo: "Corona is a premium beer, classic and authentic, recognized worldwide for its high quality, and refreshing taste. Serve with a lime wedge for an unparalleled flavour of relaxation.",
+    },
+    {
+      productName: 'Heineken',
+      productInfo: "Brewed in Holland according to the original recipe, Heineken's distinctive flavour offers a refreshing European taste that has made it a favourite all over the world.",
+    },
+    {
+      productName: 'Busch Lager',
+      productInfo: "First introduced in 1955, Busch is brewed, fermented and aged to create a smooth, refreshing taste at 4.7% alcohol by volume. Busch is an American-style lager, brewed in Canada with the Anheuser-Busch tradition of excellence.",
+    },
+    {
+      productName: 'Alexander Keiths Ipa',
+      productInfo: "Brewed in Halifax since 1820, India Pale Ale is light in colour and hopped in flavour. Only the lightest and finest barleys that produce a pale malt are used while the amounts of hops are increased so as to give a pronounced hop flavour.",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
